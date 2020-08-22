c9109690e9bda8b24c859d24cc529036710c574f
Desarrollo <onlin1992@gmail.com>
2020-08-22 01:43:55 -0500

    faltantes de componentes por realizar pie de pagina y recomendaciones

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Carrusel_principal from "./componentes/Carrusel_principal";
 import SeccionDomicilios from "./componentes/SeccionDomicilios";
 import SeccionEmpresa from "./componentes/SeccionEmpresa";
 import SeccionProductos from "./componentes/SeccionProductos";
+import SeccionExtra from "./componentes/SeccionExtra";
 import "bootstrap/dist/css/bootstrap.min.css";
 const { Header, Footer, Content } = Layout;
 
@@ -20,6 +21,114 @@ function App() {
         <SeccionDomicilios />
         <SeccionEmpresa />
         <SeccionProductos />
+        <SeccionExtra
+          items={4}
+          texto={{
+            texto: "PRODUCTOS NUEVOS",
+            fondo: "var(--color-primario)",
+            color: "white",
+          }}
+          articulos={[
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+              nombre: "ARTICULO UNO DE LOS UNOS UNOS",
+            },
+          ]}
+        />
+        <SeccionExtra
+          items={3}
+          texto={{
+            texto: "REGALOS",
+            fondo: "invisible",
+            color: "var(--color-primario)",
+          }}
+          articulos={[
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+            {
+              imagen: "img/articulodefecto.jpeg",
+              codigo: "0",
+              enlace: "##",
+            },
+          ]}
+        />
       </Content>
       <Footer></Footer>
     </Layout>
---
+++ src/componentes/SeccionExtra/index.js
@@ -0,0 +1,79 @@
+import React, { useRef } from "react";
+import Slider from "react-slick";
+import { Typography } from "antd";
+import { LeftCircleOutlined, RightCircleOutlined } from "@ant-design/icons";
+import "./styles.css";
+
+const { Text, Title } = Typography;
+function SeccionExtra({ items, articulos, texto }) {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: items,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    rtl: true,
+  };
+  let slider = useRef();
+
+  const next = () => {
+    slider.slickNext();
+  };
+  const previous = () => {
+    slider.slickPrev();
+  };
+  return (
+    <div className="seccion-extra">
+      <div
+        style={{
+          backgroundColor: texto.fondo,
+          "min-width": "100%",
+          color: texto.color,
+          textAlign: "center",
+          display: "flex",
+          "justify-content": "center",
+          alignItems: "center",
+          "font-size": "120px",
+          fontWeight: "bold",
+          marginBottom: "110px",
+        }}
+      >
+        {texto.texto}
+      </div>
+      <div className="carrousel">
+        <LeftCircleOutlined onClick={next} />
+        <div className="multi-carrousel">
+          <Slider ref={(c) => (slider = c)} {...settings}>
+            {articulos.map((articulo, i) => {
+              return (
+                <div>
+                  <div
+                    className="item"
+                    style={
+                      articulo.nombre ? {} : { padding: "0", boxShadow: "none" }
+                    }
+                  >
+                    <img
+                      src={articulo.imagen}
+                      alt={articulo.codigo}
+                      href={articulo.enlace}
+                      width={
+                        articulo.nombre ? "80%!important" : "100%!important"
+                      }
+                    />
+                    {articulo.nombre && <Text>{articulo.nombre}</Text>}
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+
+        <RightCircleOutlined color="black" onClick={previous} />
+      </div>
+    </div>
+  );
+}
+export default SeccionExtra;
