f125fb1aa21ba4c2ea360eacc80e8f5aef498dc5
Desarrollo <onlin1992@gmail.com>
2021-01-02 10:52:40 -0500

    cambios generales, falta panel de administracion

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,8 @@ const API = {
   URL: 'https://api.bodegaportilla.com/',
   APIIDFACEBOOK: '335819841014597',
   APIIDGOOGLE:
-    '389092927548-8a9viaeea3v31hvomt7icj3cm893j1uo.apps.googleusercontent.com'
+    '389092927548-8a9viaeea3v31hvomt7icj3cm893j1uo.apps.googleusercontent.com',
+
 }
 const BANCO = {
   URL: 'https://api.bodegaportilla.com/'
