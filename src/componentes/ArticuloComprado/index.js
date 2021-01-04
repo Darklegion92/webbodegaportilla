e7f9a1e49836cbc8ab5a6129d052e718e8a0db9e
Desarrollo <onlin1992@gmail.com>
2021-01-04 17:13:39 -0500

    algunos detalles finales

--- src/componentes/ArticuloComprado/index.js
+++ src/componentes/ArticuloComprado/index.js
@@ -1,53 +1,58 @@
-import React, { useState } from "react";
-import { Typography, Button, Row, Col, Divider } from "antd";
-import { Link } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
-import { BANCO } from "../../config";
-import "./styles.css";
+import React, { useState } from 'react'
+import { Typography, Button, Row, Col, Divider } from 'antd'
+import { Link } from 'react-router-dom'
+import { useMediaQuery } from 'react-responsive'
+import { BANCO } from '../../config'
+import './styles.css'
 
-const { Text } = Typography;
+const { Text } = Typography
 
 const Comprado = ({ articulo, onOk }) => {
-  const [img, setImg] = useState(BANCO.URL + articulo.img);
-  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
-  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 });
+  const [img, setImg] = useState(BANCO.URL + articulo.img)
+  const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 })
+  const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 })
 
   const onError = () => {
-    setImg("img/articulodefecto.png");
-  };
+    setImg('img/articulodefecto.png')
+  }
 
   return (
-    <div className="articulo-comprado-tag">
-      <Row className="fila1">
+    <div className='articulo-comprado-tag'>
+      <Row className='fila1'>
         <Text>TU PRODUCTO SE AGREGO AL CARRITO</Text>
         <Divider />
       </Row>
       {isTabletOrMobile || isTabletOrMobileDevice ? (
         <Col gutter={24}>
           <Col span={24}>
-            <Row justify="center">
-              <img src={img} width="70%" onError={onError} />
+            <Row justify='center' style={{ height: '30%' }}>
+              <img
+                src={img}
+                width='70%'
+                onError={onError}
+                style={{ height: '100%' }}
+              />
             </Row>
           </Col>
           <Col span={24}>
-            <Row className="fila2" justify="center">
-              <Text align="center">{articulo.nombre}</Text>
+            <Row className='fila2' justify='center'>
+              <Text align='center'>{articulo.nombre}</Text>
             </Row>
-            <Row className="fila3" justify="center">
+            <Row className='fila3' justify='center'>
               <Text>
-                ${" "}
+                ${' '}
                 {articulo.descuento > 0
                   ? Math.round(articulo.cantidad * articulo.descuento)
                   : Math.round(articulo.precio * articulo.cantidad)}
                 COP
               </Text>
             </Row>
-            <Row className="fila4" justify="center">
-              <Link to="/carshop">
+            <Row className='fila4' justify='center'>
+              <Link to='/carshop'>
                 <Button>IR AL CARRITO</Button>
               </Link>
-              <Link to="/shop">
-                <Button className="contrario" onClick={() => onOk(false)}>
+              <Link to='/shop'>
+                <Button className='contrario' onClick={() => onOk(false)}>
                   SEGUIR COMPRANDO
                 </Button>
               </Link>
@@ -58,17 +63,17 @@ const Comprado = ({ articulo, onOk }) => {
         <>
           <Row gutter={25}>
             <Col span={8}>
-              <img src={img} width="100%" onError={onError} />
+              <img src={img} width='100%' onError={onError} />
             </Col>
             <Col span={16}>
-              <Row align="middle" style={{ height: "100%" }}>
+              <Row align='middle' style={{ height: '100%' }}>
                 <Col>
-                  <Row className="fila2">
+                  <Row className='fila2'>
                     <Text>{articulo.nombre}</Text>
                   </Row>
-                  <Row className="fila3">
+                  <Row className='fila3'>
                     <Text>
-                      ${" "}
+                      ${' '}
                       {articulo.descuento > 0
                         ? Math.round(articulo.cantidad * articulo.descuento)
                         : Math.round(articulo.precio * articulo.cantidad)}
@@ -79,12 +84,12 @@ const Comprado = ({ articulo, onOk }) => {
               </Row>
             </Col>
           </Row>
-          <Row className="fila4">
-            <Link to="/carshop">
+          <Row className='fila4'>
+            <Link to='/carshop'>
               <Button>IR AL CARRITO</Button>
             </Link>
-            <Link to="/shop">
-              <Button className="contrario" onClick={() => onOk(false)}>
+            <Link to='/shop'>
+              <Button className='contrario' onClick={() => onOk(false)}>
                 SEGUIR COMPRANDO
               </Button>
             </Link>
@@ -92,7 +97,7 @@ const Comprado = ({ articulo, onOk }) => {
         </>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Comprado;
+export default Comprado
