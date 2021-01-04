6e1e43d1765cfc1e0d0d44b485b7fe14da1e7656
Desarrollo <onlin1992@gmail.com>
2021-01-04 16:54:02 -0500

    cambios finales

--- src/componentes/Articulo/index.js
+++ src/componentes/Articulo/index.js
@@ -75,7 +75,7 @@ const Articulo = ({ articulo, onOk }) => {
         </div>
       )}
       <Col span={24} className='articulo-tag' >
-        <Row justify='center' style={{height:"40px"}}>
+        <Row justify='center' style={{height:"40%"}}>
           <img src={img} width='60%' style={{height:"100%"}} onError={onError} alt={img} />
         </Row>
         <Row justify='center' className='fila2'>
