d96ea01901cc2ca172a7c2a69fc5c7be0a9a44c1
Darklegion92 <onlin199@gmail.com>
2022-07-12 20:48:03 -0500

    Organzados errores

--- src/App.js
+++ src/App.js
@@ -259,7 +259,8 @@ function App() {
             bodyStyle={{
               width: "100%",
               height: "100%",
-              padding:0
+              padding:0,
+              borderRadius: 10
             }}
           >
             <Link
@@ -269,6 +270,9 @@ function App() {
         <img
                   className="d-block w-100"
                   width="100%"
+                  style={{
+              borderRadius: 10
+            }}
                   src={
                     "../img/prom.jpg"
 
