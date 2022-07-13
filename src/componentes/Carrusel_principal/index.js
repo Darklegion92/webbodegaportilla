0b186192d595bda35fc299c2ed903e4ea185ad4c
Darklegion92 <onlin199@gmail.com>
2022-07-12 20:28:16 -0500

    Solucionado error al visualizar los carrusel

--- src/componentes/Carrusel_principal/index.js
+++ src/componentes/Carrusel_principal/index.js
@@ -12,24 +12,28 @@ import { BANCO } from "../../config";
 import "./styles.css";
 
 function Carrusel_principal() {
-  const { carrusel,grupos,consultarArticulosTienda } = useContext(GlobalContext);
+  const { carrusel, grupos, consultarArticulosTienda } = useContext(GlobalContext);
 
   const isTabletOrMobile = useMediaQuery({ maxWidth: 1224 });
   const isTabletOrMobileDevice = useMediaQuery({ maxDeviceWidth: 1224 });
-  const onClick = async(e)=>{
+  const onClick = async (e) => {
     const id = e.target.id;
-    
-    await grupos.forEach((grupo,i) => {
-        if (grupo.id == id) {
-          consultarArticulosTienda([{ nombre:grupo.nombre, tipo: "GRUPO",id }]);
-          return;
-        }
+
+    await grupos.forEach((grupo, i) => {
+      if (grupo.id == id) {
+        consultarArticulosTienda([{ nombre: grupo.nombre, tipo: "GRUPO", id }]);
+        return;
+      }
     });
-   
+
   }
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const movile = carrusel.filter(item => item.imgmovil !== null)
+  const desktop = carrusel.filter(item => item.img !== null)
+
   return (
     <div className="carrousel">
       <Carousel
@@ -47,16 +51,37 @@ function Carrusel_principal() {
         }
         interval={7000}
       >
-        {carrusel.map((item) => {
+        {isTabletOrMobile || isTabletOrMobileDevice ? movile.map((item) => {
           return (
             <Carousel.Item>
               <Link to={"/shop"} >
+
                 <img
                   className="d-block w-100"
                   src={
-                    isTabletOrMobile || isTabletOrMobileDevice
-                      ? `${BANCO.URL}img/carusel/movil/${item.imgmovil}`
-                      : `${BANCO.URL}img/carusel/${item.img}`
+
+                    `${BANCO.URL}img/carusel/movil/${item.imgmovil}`
+
+                  }
+                  onClick={onClick}
+                  alt={item.idgrupo}
+                  id={item.idgrupo}
+                />
+              </Link>
+            </Carousel.Item>
+          );
+        }):desktop.map((item) => {
+          console.log(item);
+          return (
+            <Carousel.Item>
+              <Link to={"/shop"} >
+
+                <img
+                  className="d-block w-100"
+                  src={
+
+                    `${BANCO.URL}img/carusel/${item.img}`
+
                   }
                   onClick={onClick}
                   alt={item.idgrupo}
